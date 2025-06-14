f9fe3f2b0e510d40ceb62c996922442fb87c90aa
ElectroSpark115 <pcs311325@gmail.com>
2025-06-14 15:49:55 -0500

    added new pages and added some dom population in signin.jsx

---
+++ client/pages/favorites.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Favs = () => {
+    const [favorites, setFavorites] = useState(0)
+    const [imgArr, setImgArr] = useState([]);
+
+    for (let i = 0; i < imgArr.length; i++) {
+        //will populate the dom so we can see each individual image 
+    }
+
+    //will return the populated dom
+}
