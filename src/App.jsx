01e731ac0901bac76d88e02e275ac631b6ac1077
ysabogal88 <yurisabogal@icloud.com>
2025-06-17 16:00:03 -0400

    problem with react on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Signin from '../pages/signin'; //importing from signin page
-import Signup from '../pages/signup'; //importing from signup page
-import Home from '../pages/home'; //importing from home page
+import {createRoot} from 'react-dom/client'
+//import Signin from '../Old files/client/pages/signin'; //importing from signin page
+//import Signup from '../Old files/client/pages/signup'; //importing from signup page
+import {Home} from './src/Components/home'; //importing from home page
 
 
 
@@ -11,11 +12,12 @@ const App = () => {
   return (
     <BrowserRouter> {/* allows us to traverse the different pages while still staying in our app without having to make calls to the backend */}
       <Routes> {/* this defines the different pages we are going to be visiting without interrupting the code around it */}
-        <Route path='/' element={<Signin />} /> {/* signin page nav */}
-        <Route path='/signup' element={<Signup />} /> {/*signup page nav*/}
-        <Route path='/home' element={<Home />} /> {/* home page nav */}
+        <Route path='/' element={<Home />} /> {/* signin page nav */}
+        {/* <Route path='/signup' element={<Signup />} /> signup page nav */}
+        {/* <Route path='/home' element={<Home />} /> home page nav */}
       </Routes>
     </BrowserRouter>
+    // <div>'hello word</div>
   );
 };
 
---
+++ src/Components/home.jsx
@@ -0,0 +1,20 @@
+//import react from "react"// dont need it it gets pass down
+
+const Home = ()=>{
+
+//fetch for the qoutes 
+const quotes = ['one small step for man.']
+const author = 'name'
+
+
+
+    return (
+<div>
+    <h2>{quotes}</h2>
+    <h3>{author}</h3>
+    <button>the big picture</button>
+</div>
+
+    )
+}
+export default Home
